1c6ec3bf1150b13a6e260fbe7f9a44473b1fb68e
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-10-05 10:56:27 -0700

    Importing scripts

---
+++ Assets/Scripts/Units/Player/EnemyStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float health = 100.0f;
+    public bool isAlive = true;
+    
+    public void TakeDamage(float damage) {
+        if (!isAlive) {
+            return;
+        }
+        
+        health -= damage;
+        if (health <= float.Epsilon) {
+            health = 0;
+            isAlive = false;
+        }
+    }
+}
